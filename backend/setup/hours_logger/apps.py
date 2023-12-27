4c583d685227b58e1ad4f67c8bb25e46bf5ade07
Harjot Singh <hasilon88@gmail.com>
2023-12-27 13:35:49 -0500

    Models creation

--- backend/setup/hours_logger/apps.py
+++ backend/setup/hours_logger/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class HoursLoggerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hours_logger'
--- backend/setup/hours_logger/models.py
+++ backend/setup/hours_logger/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    address = models.TextField()
+    phoneNumber = models.CharField(max_length=10)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
+
+class Shift(models.Model):
+    hours = models.FloatField()
+    price = models.FloatField()
+    total = models.FloatField(null = True, blank = True)
+    date = models.DateField()
+    description = models.TextField()
+    invoiced = models.BooleanField(default=False)
+    
+    invoice = models.ForeignKey('Invoice', on_delete=models.CASCADE, related_name='shifts')
+
+    def save(self, *args, **kwargs):
+        self.total = self.hours * self.price
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.date}"
+
+class Invoice(models.Model):
+    invoice = models.AutoField(primary_key=True)
+    date = models.DateField()
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='invoices')
+
+    def __str__(self):
+        return f"{self.date}"
