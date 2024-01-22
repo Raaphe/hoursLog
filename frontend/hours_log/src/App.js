0f6c331865aadd1dd29867f1885dcefeb61822b0
hasilon88 <hasilon88@gmail.com>
2024-01-21 19:18:15 -0500

    Changes to Login and Dashboard

--- frontend/hours_log/src/App.js
+++ frontend/hours_log/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Components/Landing_Page";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
